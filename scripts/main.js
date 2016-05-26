5caf51482fbf4ef3c3ad52691e1581567c6ab611
Elizabeth Dabbs <dabbs_elizabeth@bah.com>
2016-05-25 20:12:02 -0400

    mobile responsive

--- scripts/main.js
+++ scripts/main.js
@@ -5,16 +5,34 @@ jQuery(document).ready(function ($) {
     var pathname = window.location.pathname;
     console.log(pathname);
     if (pathname == '/photos/') {
-        $('#nav-photos').addClass('active');
+        $('.nav-photos').addClass('active');
     }
     if (pathname == '/details/') {
-        $('#nav-details').addClass('active');
+        $('.nav-details').addClass('active');
     }
     if (pathname == '/bridal-party/') {
-        $('#nav-party').addClass('active');
+        $('.nav-party').addClass('active');
     }
     if (pathname == '/our-story/') {
-        $('#nav-story').addClass('active');
+        $('.nav-story').addClass('active');
+    }
+
+    $( "#open-menu" ).click(function() {
+        openNav();
+    });
+
+    $( "#close-menu" ).click(function() {
+        closeNav();
+    });
+
+    function openNav() {
+        $('.menu-button').addClass('nav-is-visible');
+        $('#overlay').addClass('nav-is-visible');
+    }
+
+    function closeNav() {
+        $('.menu-button').removeClass('nav-is-visible');
+        $('#overlay').removeClass('nav-is-visible');
     }
 });
 
